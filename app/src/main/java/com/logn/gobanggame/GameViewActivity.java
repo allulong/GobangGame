0ca954f734b103bd74210a90bb9e63d9271323ba
allulong <allulong@outlook.com>
2017-06-02 21:29:27 +0800

    Initial commit

---
+++ app/src/main/java/com/logn/gobanggame/GameViewActivity.java
@@ -0,0 +1,18 @@
+package com.logn.gobanggame;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by long on 2017/6/2.
+ */
+
+public class GameViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_view);
+    }
+}
---
+++ app/src/main/java/com/logn/gobanggame/HomeViewActivity.java
@@ -0,0 +1,52 @@
+package com.logn.gobanggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.logn.titlebar.TitleBar;
+
+public class HomeViewActivity extends AppCompatActivity {
+
+    private TitleBar titlebar;
+    private Button person2Person;
+    private Button person2Machine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_view);
+
+        initView();
+        initValue();
+    }
+
+    private void initView() {
+        titlebar = (TitleBar) findViewById(R.id.title_bar_home);
+        person2Person = (Button) findViewById(R.id.btn_home_p2p);
+        person2Machine = (Button) findViewById(R.id.btn_home_p2m);
+    }
+
+    private void initValue() {
+        person2Person.setOnClickListener(btnListener);
+        person2Machine.setOnClickListener(btnListener);
+    }
+
+    private View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_home_p2p:
+                    Toast.makeText(HomeViewActivity.this, "对弈模式", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_home_p2m:
+                    Toast.makeText(HomeViewActivity.this, "人机对战", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(HomeViewActivity.this, "...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
