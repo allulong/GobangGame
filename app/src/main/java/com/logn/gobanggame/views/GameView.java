3f696fb0324e29a13d6fc40c9d502bb5a93d16f0
allu <allulong@outlook.com>
2017-06-03 14:36:55 +0800

    .

--- app/src/main/java/com/logn/gobanggame/views/GameView.java
+++ app/src/main/java/com/logn/gobanggame/views/GameView.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.ViewGroup;
+
+import static android.R.attr.y;
 
 /**
  * Created by long on 2017/6/2.
  */
 
 public class GameView extends ViewGroup {
+
+    private int count = 0;
 
     //线条数量
     private static final int MAX_LINE = 15;
@@ -43,11 +48,13 @@ public class GameView extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.e("流程", "onLayout" + count++);
 
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.e("流程", "onMeasure" + count++);
         //获取高宽值
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
@@ -80,6 +87,7 @@ public class GameView extends ViewGroup {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        Log.e("流程", "onSizeChanged" + count++);
 
         //拿到宽
         mPanelWidth = w;
@@ -89,8 +97,10 @@ public class GameView extends ViewGroup {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.e("流程", "onDraw" + count++);
         super.onDraw(canvas);
         drawLine(canvas);
+        drawDots(canvas);
     }
 
     /**
@@ -113,6 +123,8 @@ public class GameView extends ViewGroup {
      * @param canvas
      */
     private void drawLine(Canvas canvas) {
+
+        mPaint.setStrokeWidth(1);
         //获取高宽
         int w = mPanelWidth;
         float lineHeight = mLineHeight;
@@ -130,7 +142,41 @@ public class GameView extends ViewGroup {
             canvas.drawLine(startX, y, endX, y, mPaint);
             //绘制纵
             canvas.drawLine(y, startX, y, endX, mPaint);
+
+            Log.e("坐标", "" + startX + "::" + endX + "::" + y);
         }
+
+    }
+
+    private void drawDots(Canvas canvas) {
+        mPaint.setStrokeWidth(6);
+
+        float lineHeight = mLineHeight;
+
+        float certenDot = 0;
+        float dot1 = 0;
+        float dot2 = 0;
+
+        //遍历，绘制线条
+        for (int i = 0; i < MAX_LINE; i++) {
+            if (i == 3) {
+                dot1 = (int) ((0.5 + i) * lineHeight);
+            }
+            if (i == 7) {
+                certenDot = (int) ((0.5 + i) * lineHeight);
+            }
+            if (i == 11) {
+                dot2 = (int) ((0.5 + i) * lineHeight);
+            }
+        }
+
+        //绘画天元点
+        canvas.drawCircle(certenDot, certenDot, 3, mPaint);
+
+        canvas.drawCircle(dot1, dot1, 3, mPaint);
+        canvas.drawCircle(dot2, dot2, 3, mPaint);
+        canvas.drawCircle(dot1, dot2, 3, mPaint);
+        canvas.drawCircle(dot2, dot1, 3, mPaint);
 
     }
 }
